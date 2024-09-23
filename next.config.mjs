81b64a2f6e46a603616bf580ec8c924c9fd4398b
jeden1102 <bum123@o2.pl>
2024-09-23 21:35:58 +0200

    master. Removed exprerimental config options.

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@ import { withSentryConfig } from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  experimental: { serverActions: true },
 };
 
 export default withSentryConfig(nextConfig, {
